806c50fd5358d016c2f07e1acaf638cc5a32a5b0
Jesse Theisen <theiseja@gmail.com>
2019-03-19 20:49:03 -0500

    MVP: Endpoints created

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const db = knex(knexConfig.development);
 const server = express();
 const PORT = 9090;
 
+const cohortRouter = require('./data/routes/cohortRouter')
+const studentRouter = require('./data/routes/studentRouter')
+
 server.use(express.json());
+
+server.use('/api/students', studentRouter)
+server.use('/api/cohorts', cohortRouter)
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
