632a4c9caa3f1f8436621c81dc533fc8c48c8004
Jesse Theisen <theiseja@gmail.com>
2019-03-19 20:09:43 -0500

    MVP: Project setup & created new branch

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const express = require("express");
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 const db = knex(knexConfig.development);
 const server = express();
-const PORT = 5000;
+const PORT = 9090;
 
 server.use(express.json());
 
 server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
